694664531870ab07e3f428bb34bebbe08d21cded
alex <alex@alwa.com>
2022-02-03 21:47:35 +0000

    Fixed formatting

--- src/test/java/examples/MyStepdefs.java
+++ src/test/java/examples/MyStepdefs.java
@@ -97,18 +97,16 @@ public class MyStepdefs {
 
     @After
     public void afterScenario(Scenario scenario) {
-
-            try {
-                if (scenario.isFailed()) {
-                    String scnShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
-                    String screenshot = "data:image/gif;base64, " + scnShot;
-                    scenario.attach(screenshot, "gif;base64", "screenshot");
-                }
-            } catch (Exception ignored) {
-            
-            } finally {
-                driver.close();
+        try {
+            if (scenario.isFailed()) {
+                String scnShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+                String screenshot = "data:image/gif;base64, " + scnShot;
+                scenario.attach(screenshot, "gif;base64", "screenshot");
             }
+        } catch (Exception ignored) {
+
+        } finally {
+            driver.close();
         }
     }
 
